28a1f831046e4a8c0e673fbc0dffbdf7947716f3
mingreen0107 <71gnos@naver.com>
2023-04-02 16:17:14 +0900

    실습

---
+++ 소스2.c
@@ -0,0 +1,17 @@
+/*public class gg {
+	public static void main(String[] args) {
+		int dan;
+		int times;
+
+		for (dan = 2; dan <= 9; dan++)
+		{
+			for (times = 1; times <= 9; times++)
+			{
+				System.out.println(dan + "X" + times + "=" + dan * times);
+			}
+		}
+		System.out.println();
+		//2단부터 9단까지 한달락 띄고 출력됨.
+	}
+}*/
+printf("실습");
